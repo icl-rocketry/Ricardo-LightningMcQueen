4bb823b9de4dcb7cf346d94779d9ce2d5fae9512
kiran <kd619@ic.ac.uk>
2022-08-25 19:29:40 +0100

    updated pins and new layout

--- Powerboard/Software/include/ricardo_pins.h
+++ Powerboard/Software/include/ricardo_pins.h
@@ -5,27 +5,60 @@
  */
 #pragma once
 
+// static constexpr int BaroCs = 2;
+// static constexpr int LoraReset = 4;
+// static constexpr int LoraCs = 5;
+// static constexpr int LoraInt = 6;
+
+// static constexpr int _SDA = 9;
+// static constexpr int _SCL = 32;
+
+// static constexpr int PDU_EN = 26;    //should be 9
+
+// static constexpr int TxCan = 18;
+// static constexpr int RxCan = 5;
+
+// static constexpr int BattStat1 = 14;    //should be 14
+// static constexpr int BattStat2 = 27;    //should be 27
+
+// static constexpr int Stat1_3V3 = 32;
+// static constexpr int Stat2_3V3 = 25;
+
+// static constexpr int LPC_24V = 4;  //should be 4
+
+// static constexpr int Charge = 23;
+
+// static constexpr int PG = 10;   //should be 26
+
+
+// /*
+// **********************
+// * PINS               *
+// **********************
+//  */
+// #pragma once
+
 static constexpr int BaroCs = 2;
 static constexpr int LoraReset = 4;
 static constexpr int LoraCs = 5;
 static constexpr int LoraInt = 6;
 
-static constexpr int _SDA = 9;
-static constexpr int _SCL = 32;
+static constexpr int _SDA = 21;
+static constexpr int _SCL = 22;
 
-static constexpr int PDU_EN = 26;    //should be 9
+static constexpr int PDU_EN = 9;
 
 static constexpr int TxCan = 18;
 static constexpr int RxCan = 5;
 
-static constexpr int BattStat1 = 14;    //should be 14
-static constexpr int BattStat2 = 27;    //should be 27
+static constexpr int BattStat1 = 14;
+static constexpr int BattStat2 = 27;
 
 static constexpr int Stat1_3V3 = 32;
 static constexpr int Stat2_3V3 = 25;
 
-static constexpr int LPC_24V = 4;  //should be 4
+static constexpr int LPC_24V = 33;
 
 static constexpr int Charge = 23;
 
-static constexpr int PG = 10;   //should be 26
+static constexpr int PG = 26;
