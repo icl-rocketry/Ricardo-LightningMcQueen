e47bde50942e9f4f996d7fffdacd1ff52d4e75cf
Artem <artem.sheykin20@imperial.ac.uk>
2022-08-30 13:17:37 +0100

    pdu enable fix

--- Powerboard/Software/src/States/idle.cpp
+++ Powerboard/Software/src/States/idle.cpp
@@ -20,11 +20,17 @@ void Idle::initialise(){
 State* Idle::update(){
     Serial.println("Idle");
 
-    if(digitalRead(PDU_EN) == HIGH){ //if PDU enable connector pulled out
-        Serial.println("PDU_EN is HIGH");
-        State* timeout_ptr = new Timeout(_sm);  //transition to 'timeout' state
+    if(digitalRead(PDU_EN) == HIGH){    //if PDU_EN pin externally pulled high because conn was inserted 
+        key_was_inserted == true;       //bool var is true
+    }
+
+    if(digitalRead(PDU_EN) == LOW && key_was_inserted == true){ //if conn pulled out and hence internally
+        Serial.println("PDU_EN is HIGH");                       //pulled low, transition to 'timeout' state
+        State* timeout_ptr = new Timeout(_sm); 
         return timeout_ptr;
-    }else{
+    }
+
+    else{
         return this; //loopy loop
     }
 
